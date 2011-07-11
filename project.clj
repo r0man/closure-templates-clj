3bd3ea3b5bad00b158e6d1841374118a2bee9a15
Roman <roman@burningswell.com>
2011-07-12 00:08:18 +0200

    Added autodoc to dev dependencies.

--- project.clj
+++ project.clj
@@ -1,8 +1,11 @@
 (defproject closure-templates-clj "0.0.1-SNAPSHOT"
-  :description "Closure Templates"
+  :author "Roman Scherer"
+  :autodoc {:copyright "Copyright (c) 2010 Roman Scherer" :name "Closure Templates"}
+  :description "Clojure Library for Google's Closure Templates."
+  :url "https://github.com/r0man/closure-templates-clj"
   :dependencies [[clj-soy/google-closure-templates "20100708"]
                  [inflections "0.4.3"]
                  [org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [org.slf4j/slf4j-log4j12 "1.5.10"]]
-  :dev-dependencies [[swank-clojure "1.3.1"]])
+  :dev-dependencies [[org.clojars.rayne/autodoc "0.8.0-SNAPSHOT"]])
