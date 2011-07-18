af76527d6d41d84f3375cae6ffaa7bcd079d2b7c
Roman <roman@burningswell.com>
2011-07-18 23:39:50 +0200

    Compile an Object before rendering to get rid of all other cases.

--- src/closure/templates/render.clj
+++ src/closure/templates/render.clj
@@ -1,10 +1,7 @@
 (ns closure.templates.render
   (:refer-clojure :exclude (compile))
-  (:import [clojure.lang IPersistentSet IPersistentVector ISeq]
-           [java.net URI URL]
-           java.io.File com.google.template.soy.tofu.SoyTofu)
-  (:use [clojure.contrib.string :only (as-str)]
-        [clojure.walk :only (stringify-keys)]
+  (:import java.io.File com.google.template.soy.tofu.SoyTofu)
+  (:use [clojure.walk :only (stringify-keys)]
         [inflections.core :only (underscore-keys)]
         closure.templates.compile))
 
@@ -17,42 +14,12 @@
   (render [_ _ _ _]
     nil))
 
-(extend-type File
+(extend-type Object
   Render
   (render [file template data bundle]
     (render (compile file) template data bundle)))
-
-(extend-type IPersistentSet
-  Render
-  (render [set template data bundle]
-    (render (seq set) template data bundle)))
-
-(extend-type IPersistentVector
-  Render
-  (render [vector template data bundle]
-    (render (seq vector) template data bundle)))
-
-(extend-type ISeq
-  Render
-  (render [seq template data bundle]
-    (render (compile seq) template data bundle)))
 
 (extend-type SoyTofu
   Render
   (render [tofu template data bundle]
     (.render tofu template (underscore-keys (stringify-keys data)) bundle)))
-
-(extend-type String
-  Render
-  (render [path template data bundle]
-    (render (compile path) template data bundle)))
-
-(extend-type URI
-  Render
-  (render [uri template data bundle]
-    (render (File. uri) template data bundle)))
-
-(extend-type URL
-  Render
-  (render [url template data bundle]
-    (render (.toURI url) template data bundle)))
