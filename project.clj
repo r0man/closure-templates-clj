53dd3bbc292ef610b1634ba82bef997e31170c25
Roman <roman@burningswell.com>
2011-07-22 19:14:36 +0200

    Removed log4j dependency.

--- project.clj
+++ project.clj
@@ -9,6 +9,5 @@
   :dependencies [[clj-soy/google-closure-templates "20100708"]
                  [inflections "0.4.4"]
                  [org.clojure/clojure "1.2.1"]
-                 [org.clojure/clojure-contrib "1.2.0"]
-                 [org.slf4j/slf4j-log4j12 "1.5.10"]]
+                 [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[org.clojars.rayne/autodoc "0.8.0-SNAPSHOT"]])
