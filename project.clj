61ccffaa7c3892de5f1c59f1b7f8883575146a63
Roman <roman@burningswell.com>
2011-07-22 17:44:16 +0200

    Indentation.

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
 (defproject closure-templates-clj "0.0.1-SNAPSHOT"
   :author "Roman Scherer"
-  :autodoc
-  {:copyright "Copyright (c) 2010 Roman Scherer"
-   :name "Clojure Library for Google's Closure Templates"
-   :web-src-dir "https://github.com/r0man/closure-templates-clj/blob/"
-   :web-home "https://r0man.github.com/closure-templates-clj/"}
+  :autodoc {:copyright "Copyright (c) 2011 Roman Scherer"
+            :name "Clojure Library for Google's Closure Templates"
+            :web-src-dir "https://github.com/r0man/closure-templates-clj/blob/"
+            :web-home "https://r0man.github.com/closure-templates-clj/"}
   :description "Clojure Library for Google's Closure Templates."
   :url "https://github.com/r0man/closure-templates-clj"
   :dependencies [[clj-soy/google-closure-templates "20100708"]
