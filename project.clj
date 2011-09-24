c373485b0f2ae3386772fc1630d68ffa8b358dc5
Roman <roman@burningswell.com>
2011-09-24 17:31:58 +0200

    Started 0.1.1-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject closure-templates-clj "0.1.0"
+(defproject closure-templates-clj "0.1.1-SNAPSHOT"
   :author "Roman Scherer"
   :autodoc {:copyright "Copyright (c) 2011 Roman Scherer"
             :name "Clojure Library for Google's Closure Templates"
