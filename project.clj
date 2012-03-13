e88c72733e232286d0effa0201db9fa201159cfa
Roman <roman@burningswell.com>
2012-03-13 21:39:14 +0100

    Switched to inflections "0.7.0-SNAPSHOT".

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :description "Clojure Library for Google's Closure Templates."
   :url "https://github.com/r0man/closure-templates-clj"
   :dependencies [[clj-soy/google-closure-templates "20100708"]
-                 [inflections "0.6.5-SNAPSHOT"]
+                 [inflections "0.7.0-SNAPSHOT"]
                  [org.clojure/clojure "1.3.0"]]
   :min-lein-version "2.0.0")
